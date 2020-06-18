c91dbed43049be50ae8ae9f1e56ed233ff725985
CoolSummer <541456313@qq.com>
2020-06-19 00:18:29 +0800

    合并字符串

---
+++ src/com/denggeniubi/www/ListNode.java
@@ -0,0 +1,23 @@
+package com.denggeniubi.www;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    @Override
+    public String toString() {
+        return "ListNode{" +
+                "val=" + val +
+                ", next=" + next +
+                '}';
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+}
